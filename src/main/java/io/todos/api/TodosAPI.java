6ef850b7614efbce43c2ca69c34b59ef554e12a7
corbs <corbsmartin@gmail.com>
2019-07-30 16:34:39 -0500

    adding logging for log demo

--- src/main/java/io/todos/api/TodosAPI.java
+++ src/main/java/io/todos/api/TodosAPI.java
@@ -41,79 +41,89 @@ public class TodosAPI {
 
     @GetMapping("/")
     public List<Todo> retrieve() {
+        LOG.debug("Retrieving all todos as a List<Todo> of size " + todos.size()
+            + " todos.api.limit=" + properties.getApi().getLimit());
         return new ArrayList<>(todos.values());
     }
 
     @PostMapping("/")
     public Todo create(@RequestBody Todo todo) {
         if(todos.size() < properties.getApi().getLimit()) {
+            LOG.debug("Todos list size=" + todos.size() + " is less than todos.api.limit=" + properties.getApi().getLimit());
             if(properties.getIds().getTinyId()) {
+                LOG.debug("Using tinyId generation");
                 todo.setId(UUID.randomUUID().toString().substring(0, 8));
             } else {
+                LOG.debug("Using uuId generation");
                 todo.setId(UUID.randomUUID().toString());
             }
-            LOG.info("Saving Todo " + todo + " into map");
+            LOG.debug("Generated todo.id=" + todo.getId());
+            LOG.trace("Saving Todo " + todo + " into map");
             todos.put(todo.getId(), todo);
             return todos.get(todo.getId());
         } else {
+            LOG.error("Limit reached, todos list size=" + todos.size()
+                + " is >= todos.api.limit=" + properties.getApi().getLimit());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                    format("todos.api.limit=%d, todos.size()=%d", properties.getApi().getLimit(), todos.size()));
+                format("todos.api.limit=%d, todos.size()=%d", properties.getApi().getLimit(), todos.size()));
         }
     }
 
     @DeleteMapping("/")
     public void delete() {
+        LOG.info("Removing ALL " + todos.size() + " todos.");
         todos.clear();
     }
 
     @GetMapping("/{id}")
     public Todo retrieve(@PathVariable String id) {
         if(!todos.containsKey(id)) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, format("todo.id=%d", id));
+            LOG.warn("Whatcha talkin bout Willis?  todo.id=" + id + " NOT FOUND.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, format("todo.id=%s", id));
         }
+        LOG.trace("Retrieving todo.id=" + id);
         return todos.get(id);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable String id) {
+        if(!todos.containsKey(id)) {
+            LOG.warn("Whatcha talkin bout Willis?  Can't delete a todo that doesn't exist todo.id=" + id);
+            return;
+        }
+        LOG.info("Removing todo.id=" + id);
         todos.remove(id);
-
     }
 
     @PatchMapping("/{id}")
     public Todo update(@PathVariable String id, @RequestBody Todo todo) {
         if(todo == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "todo can't be null");
+            LOG.error("Todo RequestBody can't be null...inconceivable!");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "todo request body can't be null");
         }
         if(!todos.containsKey(id)) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, format("todo.id=%d", id));
+            LOG.warn("Whatcha talkin bout Willis?  Can't update a todo that doesn't exist todo.id=" + id);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, format("todo.id=%s", id));
         }
         Todo current = todos.get(id);
+        LOG.trace("Pulled current todo.id=" + current.getId() + " from todos List.");
         if(!ObjectUtils.isEmpty(todo.getComplete())) {
+            LOG.trace("Updating todo.id=" + current.getId() + " complete field from "
+                + current.getComplete() + " to " + todo.getComplete());
             current.setComplete(todo.getComplete());
         }
         if(!StringUtils.isEmpty(todo.getTitle())){
+            LOG.trace("Updating todo.id=" + current.getId() + " title field from "
+                    + current.getTitle() + " to " + todo.getTitle());
             current.setTitle(todo.getTitle());
         }
+        LOG.trace("Returning updated todo for todo.id=" + current.getId());
         return current;
     }
 
     @GetMapping("/limit")
     public Limit getLimit() {
+        LOG.info("Checking Limit, todos.size=" + this.todos.size() + " todos.api.limit=" + properties.getApi().getLimit());
         return Limit.builder().size(this.todos.size()).limit(properties.getApi().getLimit()).build();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
